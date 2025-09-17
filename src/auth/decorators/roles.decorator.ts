a9f4641cbb467a54c3f24280b343ca5bc41d9c79
Laizinhavivi <vitorialaiza605@gmail.com>
2025-09-17 15:55:17 -0300

    17/09/2025

--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,4 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '../../common/enum/role.enum';
+
+// Esta é a chave que usaremos para armazenar e recuperar os metadados
 export const ROLES_KEY = 'roles';
+
+// A função Roles recebe uma lista de papéis e os anexa aos metadados da rota
 export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
